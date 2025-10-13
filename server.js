5f8c3143f7daabb21a1a6f9ef21a9e174b0025d8
Vikash Mishra <mishravikash0612@gmail.com>
2025-10-13 15:05:47 +0530

    Initialized Socket.io

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 const generateResponse = require('./src/services/ai.service')
+const initSocketServer = require('./src/sockets/socketServer')
+const httpServer = require('http').createServer(app)
 
 connectDB();
+initSocketServer(httpServer)
 
-app.listen(3000, () => {
+httpServer.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
