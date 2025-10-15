2cea5cfdf63783d90eef33e0bdbdee10a04c85c9
Vikash Mishra <mishravikash0612@gmail.com>
2025-10-15 21:00:15 +0530

    `Added dependencies @google/genai and @google/generative-ai, updated package-lock.json and package.json, and modified server.js to include socket.io and database connection.`

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const app = require("./src/app");
+const initSocketServer = require("././src/sockets/socket.server");
+const connectDb = require("./src/db/db");
+const httpServer = require("http").createServer(app);
 
-app.listen(3000, () => {
+connectDb();
+initSocketServer(httpServer);
+
+httpServer.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
-const connectDb = require('./db/db');
-connectDb()
+
 const app = express();
 
 /* Routes */
